e8e0ecc506485d6bb45c1d4d7aee93d901b287f4
hahnnz <lphion96@naver.com>
2019-04-24 13:09:11 +0900

    update and deleleted sensitivity, specitivity

--- jihoon/train_SEMIC.py
+++ jihoon/train_SEMIC.py
@@ -10,6 +10,9 @@ net = inception_v4.create(data_shape=(79, 95, 79, 1), num_output=2, mode='classi
 
 num_epochs = 1000
 lowest_loss=None
+highest_auc=None
+
+output_list = [net.output, net.loss]
 
 with net.sess as sess:
     with tf.device('/gpu:0'):
@@ -20,32 +23,33 @@ with net.sess as sess:
         
         for epoch in range(num_epochs):
 
+            feed_dict = {phase : {} for phase in ['train', 'valid']}
             prediction = {phase : [] for phase in ['train', 'valid']}
             loss = {phase : 0 for phase in ['train', 'valid']}
             count = {phase : 0 for phase in ['train', 'valid']}
-            metric = {method : 0 for method in ['auc', 'sensitivity', 'specitivity']}
-            for method in metric.keys():
-                metric[method] = {phase : 0 for phase in ['train', 'valid']}
+            auc = {phase : 0 for phase in ['train', 'valid']}
             
             for phase in ['train', 'valid']:
                 for _ in range(len(semic.train_subj_list if phase == 'train' else semic.valid_subj_list)):
                     semic.load(phase)
-
+                    
+                    feed_dict = {net.x: semic.batchset['fmri'][i],
+                                 net.y: semic.batchset['labels'][i],
+                                 net.keep_prob: 0.7 if phase=='train' else 1.0,
+                                 net.is_train : True if phase=='train' else False}
+                    
+                    if phase == 'train' : feed_dict.update({net.lr : 1. })
+                    
                     for i in range(len(semic.batchset['labels'])):
-                        pred, cost, op = sess.run([net.output, net.loss,net.train_op], 
-                                                  feed_dict={net.x: semic.batchset['fmri'][i],
-                                                             net.y: semic.batchset['labels'][i], 
-                                                             net.lr : 1.,
-                                                             net.keep_prob: 0.7,
-                                                             net.is_train: True})
+                        if phase == 'train' :
+                            pred, cost, _ = sess.run(output+[net.train_op], feed_dict=feed_dict)
+                        else :
+                            pred, cost = sess.run(output_list, feed_dict=feed_dict)
 
                         pred = pred.argmax(-1)
                         fpr, tpr, _ = metrics.roc_curve(semic.batchset['labels'][i], pred, pos_label=1)
-                        tn, fp, fn, tp = metrics.confusion_matrix(semic.batchset['labels'][i], pred).ravel()
                         
-                        metric['auc'][phase] += metrics.auc(fpr, tpr)
-                        metric['sensitivity'][phase] += tp / (tp+fn)
-                        metric['sensitivity'][phase] += tn / (tn+fp)
+                        auc[phase] += metrics.auc(fpr, tpr)
                         loss[phase] += cost
                         count[phase] += 1
             
@@ -53,14 +57,15 @@ with net.sess as sess:
             for phase in ['train', 'valid']:
                 summ.value.add(tag=phase+'_loss', 
                                simple_value=loss[phase]/count[phase] if count[phase] !=0 else 0)
-                
-                for method in ['auc', 'sensitivity', 'specitivity']:
-                    summ.value.add(tag=phase+'_'+method, 
-                                   simple_value=metric[method][phase]/count[phase] if count[phase] !=0 else 0)
+                summ.value.add(tag=phase+'_auc', 
+                               simple_value=auc[phase]/count[phase] if count[phase] !=0 else 0)
                     
             summary_writer.add_summary(summ,epoch)
             
-            if epoch > 0:
-                if lowest_loss == None or lowest_loss > loss['valid']/count['valid'] :
-                    lowest_loss = loss['valid']/count['valid']
-                    saver.save(sess, "./out/SEMIC/inception.ckpt")
+            if epoch > 0 and phase == 'valid':
+                if lowest_loss == None or owest_loss > loss[phase]/count[phase] :
+                    lowest_loss = loss[phase]/count[phase]
+                    saver.save(sess, "./out/SEMIC/inception_lowest_loss.ckpt")
+                if highest_auc == None or highest_auc < auc[phase]/count[phase] :
+                    lowest_loss = auc[phase]/count[phase]
+                    saver.save(sess, "./out/SEMIC/inception_highest_auc.ckpt")
