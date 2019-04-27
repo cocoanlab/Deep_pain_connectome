da6905ff5a9ea2a0650b14d9b59ce46ab908b816
hahnnz <lphion96@naver.com>
2019-04-27 10:46:24 +0900

    disabled converting nii to npy

--- jihoon/scripts/convert_nii_to_npy.py
+++ jihoon/scripts/convert_nii_to_npy.py
@@ -4,10 +4,10 @@ import os, pickle
 
 from multiprocessing import Pool
 
-def convert_nii_data(os_walk):
+def convert_nii_to_npy(os_walk):
     dirpath, dirnames, filenames = os_walk
     if 'sub-semic' in dirpath and '@eaDir' not in dirpath:
-        nifti_path = [path for path in filenames if 'nii' in path]
+        nifti_path = [path for path in filenames if 'nii' in path and 'beta' in path]
         for path in nifti_path:
             data = sitk.ReadImage('/'.join([dirpath,path]))
             data = sitk.GetArrayFromImage(data)
@@ -20,25 +20,6 @@ def convert_nii_data(os_walk):
                 
             np.save(numpy_path, data)
 
-            # save fmri data as pickle with making it dict
-            pickle_path = dirpath.split('/')
-            pickle_path.insert(-2, 'pickle_data')
-            if not os.path.exists('/'.join(pickle_path)) : os.makedirs('/'.join(pickle_path))
-            pickle_path = '/'.join(pickle_path+[path.split('.')[0]+'.pickle'])
-            
-            x,y,z = data.shape
-
-            fmri = {'voxels':[], 'locations':[], 'orig_shape':data.shape}
-
-            for i in range(x):
-                for j in range(y):
-                    for k in range(z):
-                        if str(data[i,j,k]) != 'nan':
-                            fmri['voxels'].append(data[i,j,k])
-                            fmri['locations'].append([i,j,k])
-                            
-            with open(pickle_path, 'wb') as handle:
-                pickle.dump(fmri, handle, protocol=pickle.HIGHEST_PROTOCOL)
     else : pass
     
 dataset_path = ["/cocoanlab2/GPU1_sync/data/SEMIC/imaging/model02_FIR_SPM_SINGLE_TRIAL/",
