8c3a285263cc7ca2d3199ef1a866b2b48e610fcb
hahnnz <lphion96@naver.com>
2019-04-28 19:20:18 +0900

    upgrade semic data processing

--- jihoon/scripts/SEMIC.py
+++ jihoon/scripts/SEMIC.py
@@ -1,27 +1,9 @@
 import pandas as pd
 import numpy as np
-import SimpleITK as sitk
-import os, glob
+import os
 
 from multiprocessing import Pool
 from tqdm import tqdm
-
-def explore_dir(dir,count=0,f_extensions=None):
-    if count==0:
-        global n_dir, n_file, filenames, filelocations
-        n_dir=n_file=0
-        filenames=list()
-        filelocations=list()
-
-    for img_path in sorted(glob.glob(os.path.join(dir,'*' if f_extensions is None else '*.'+f_extensions))):
-        if os.path.isdir(img_path):
-            n_dir +=1
-            explore_dir(img_path,count+1)
-        elif os.path.isfile(img_path):
-            n_file += 1
-            filelocations.append(img_path)
-            filenames.append(img_path.split("/")[-1])
-    return np.array((filenames,filelocations))
 
 class load_dataset:
     def __init__(self, dataset_path='/data/SEMIC/', workers=4, shuffle=False, split_ratio=0.8, batch_size = 20):
@@ -34,39 +16,46 @@ class load_dataset:
         semic_rest_path = []
 
         self.__label_list = ['model02_Overall_FIR_SPM_SINGLE_TRIAL', 'model02_FIR_SPM_SINGLE_TRIAL']
+        self.__label_names = ['rest', 'pain']
+        self.__phase_list = ['train', 'valid']
 
-        self.semic_path = {name : {} for name in ['semic', 'control']}
-
-        for path in explore_dir(dataset_path)[1]:
-            for subj_num in path.split('/'):
+        self.semic_path = {name : {} for name in self.__label_names}
+        
+        for dirpath, _, filenames in os.walk(dataset_path):
+            for subj_num in dirpath.split('/'):
                 if 'sub-semic' in subj_num:
                     subj_num = ''.join([l for l in subj_num if ord(l) >= 48 and ord(l) <= 57])
+
+                    if subj_num != '017': # subject 17 has a problem
+                        try : 
+                            for name in self.__label_names : 
+                                self.semic_path[name][subj_num]
+                        except : 
+                            for name in self.__label_names : 
+                                self.semic_path[name][subj_num] = []
+                        for path in filenames:
+                            path = '/'.join([dirpath, path])
+                            if 'beta' in path and self.__label_list[0] in path :
+                                self.semic_path[self.__label_names[0]][subj_num].append(path)
+                            elif 'beta' in path and self.__label_list[1] in path :
+                                self.semic_path[self.__label_names[1]][subj_num].append(path)
                     break
-            if subj_num != '017': # subject 17 has a problem
-                try : 
-                    for name in ['semic', 'control'] : 
-                        self.semic_path[name][subj_num]
-                except : 
-                    for name in ['semic', 'control'] : 
-                        self.semic_path[name][subj_num] = []
 
-                if 'beta' in path and self.__label_list[0] in path : self.semic_path['control'][subj_num].append(path)
-                elif 'beta' in path and self.__label_list[1] in path : self.semic_path['semic'][subj_num].append(path)
-
-        subj_list = list(self.semic_path['control'])
+        subj_list = list(self.semic_path[self.__label_names[0]])
         num_valid = len(subj_list) - int(len(subj_list) * split_ratio)
-
+        
         self.valid_subj_list = list(np.random.choice(subj_list, num_valid))
         self.train_subj_list = [num for num in subj_list if num not in self.valid_subj_list]
 
-        self.__semic_sampled_idx = list(np.random.choice(range(len(self.semic_path['semic'][self.train_subj_list[0]])),
-                                                         len(self.semic_path['control'][self.train_subj_list[0]]),
-                                                         replace=False))
+        self.__semic_sampled_idx = list(np.random.choice(
+            range(len(self.semic_path[self.__label_names[1]][self.train_subj_list[0]])),
+            len(self.semic_path[self.__label_names[0]][self.train_subj_list[0]]),
+            replace=False))
         
         print('Train dataset Subject list : ' + ', '.join(self.train_subj_list) + '\n')
         print('Validation dataset Subject list : ' + ', '.join(self.valid_subj_list) + '\n')
 
-    def read_nii(self, path):
+    def read_npy(self, path):
         for label in path.split('/'):
             if label in self.__label_list:
                 label = self.__label_list.index(label)
@@ -79,36 +68,36 @@ class load_dataset:
         if label == 1 and file_num not in self.__semic_sampled_idx : 
             return None, None
         else : 
-            data = sitk.ReadImage(path)
-            data = sitk.GetArrayFromImage(data)
-            return data, label
+            return np.load(path), label
             
     def load(self, phase):
-        if phase not in ['train', 'valid'] : raise ValueError('phase must be "train" or "valid".')
+        if phase not in self.__phase_list : raise ValueError('phase must be "train" or "valid".')
         phase = phase.lower()
             
         try : 
             self.current_subj
             del self.batchset
-        except : self.current_subj = {name : 0 for name in ['train', 'valid']}
+        except : self.current_subj = {name : 0 for name in self.__phase_list}
 
         subj_num = self.train_subj_list[self.current_subj[phase]] if phase == 'train' else self.valid_subj_list[self.current_subj[phase]]
 
         loaded_dataset = {
-            name : Pool(processes=self.workers).map(self.read_nii, self.semic_path[name][subj_num])
-            for name in ['semic', 'control']
+            name : Pool(processes=self.workers).map(self.read_npy, self.semic_path[name][subj_num])
+            for name in self.__label_names
         }
 
-        fmri_data = []
-        fmri_label = []
-        for name in ['semic', 'control']:
+        for name in self.__label_names:
+            fmri_data = []
+            fmri_label = []
+            
             for fmri, label in loaded_dataset[name]:
                 if label != None:
                     fmri_data.append(fmri[np.newaxis])
                     fmri_label.append([label])
-
-        fmri_data = np.concatenate(fmri_data, axis=0)[:,:,:,:,np.newaxis]
-        fmri_label = np.concatenate(fmri_label, axis=0)
+                    
+            loaded_dataset[name] = {}
+            loaded_dataset[name]['fmri'] = np.concatenate(fmri_data, axis=0)[:,:,:,:,np.newaxis]
+            loaded_dataset[name]['labels'] = np.concatenate(fmri_label, axis=0)
 
         if self.current_subj[phase]+1 == len(self.train_subj_list if phase == 'train' else self.valid_subj_list):
             self.current_subj[phase] = 0
@@ -116,23 +105,42 @@ class load_dataset:
             self.current_subj[phase]+=1
 
         if self.shuffle :
-            indices = np.random.permutation(len(fmri_label))
-            fmri_data = fmri_data[indices]
-            fmri_label = fmri_label[indices]
-
-
+            indices = np.random.permutation(len(loaded_dataset[self.__label_names[-1]]['labels']))
+            for name in self.__label_names:
+                loaded_dataset[name]['fmri'] = loaded_dataset[name]['fmri'][indices]
+                loaded_dataset[name]['labels'] = loaded_dataset[name]['labels'][indices]
+            
         self.num_batchs = int(len(fmri_label)/self.batch_size)+1 if len(fmri_label) % self.batch_size !=0 else int(len(fmri_label)/self.batch_size)
 
         self.batchset = {name : [] for name in ['fmri', 'labels']}
 
         for n in range(self.num_batchs):
-            start = n*self.batch_size
-            end = (n+1)*self.batch_size
+            start = n*(self.batch_size/2)
+            end = (n+1)*(self.batch_size/2)
+            start, end = map(int,[start,end])
             if len(fmri_label) % self.batch_size !=0:
-                self.batchset['fmri'].append(fmri_data[start:end] if n != self.num_batchs-1 else fmri_data[start:])
-                self.batchset['labels'].append(fmri_label[start:end] if n != self.num_batchs-1 else fmri_label[start:])
+                fmri = []
+                label = []
+                for name in self.__label_names:
+                    if n != self.num_batchs-1:
+                        fmri.append(loaded_dataset[name]['fmri'][start:end])
+                        label.append(loaded_dataset[name]['labels'][start:end])
+                    else : 
+                        fmri.append(loaded_dataset[name]['fmri'][start:])
+                        label.append(loaded_dataset[name]['labels'][start:])
+                
             elif len(fmri_label) % self.batch_size ==0:
-                self.batchset['fmri'].append(fmri_data[start:end])
-                self.batchset['labels'].append(fmri_label[start:end])
+                fmri = []
+                label = []
+                for name in self.__label_names:
+                    fmri.append(loaded_dataset[name]['fmri'][start:end])
+                    label.append(loaded_dataset[name]['labels'][start:end])
+                    
+            self.batchset['fmri'].append(np.concatenate(fmri, axis=0))
+            self.batchset['labels'].append(np.concatenate(label, axis=0))
+            
+            indices = np.random.permutation(len(self.batchset['labels'][-1]))
+            for name in ['fmri', 'labels']:
+                self.batchset[name][-1] = self.batchset[name][-1][indices]
 
-        del fmri_data, fmri_label
+        del loaded_dataset
