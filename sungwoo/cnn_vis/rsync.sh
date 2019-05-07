baf43a3e30910bb9a9e66e903f5d520f129fb098
Sungwoo-Lee <sungwoo320@gmail.com>
2019-05-07 13:49:40 +0900

    updates

---
+++ sungwoo/cnn_vis/rsync.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rsync -auvh sungwoolee@115.145.189.217:/Users/sungwoo320/Dropbox/Programming_Study/Tensorflow_Study/Tensorflow-Bootcamp-master/03-Convolutional-Neural-Networks /home/cocoan-gpu1/Documents/Deep_pain_connectome/sungwoo/cnn_vis
