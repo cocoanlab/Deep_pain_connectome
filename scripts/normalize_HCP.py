f524e0dde673055b984815022fee3418195c394f
hahnnz <lphion96@naver.com>
2019-10-01 15:55:31 +0900

    added normalization result validation

--- scripts/normalize_HCP.py
+++ scripts/normalize_HCP.py
@@ -7,7 +7,7 @@ from nilearn.input_data import NiftiMasker
 from nilearn.masking import apply_mask, unmask
 from multiprocessing import Pool
 
-def normalize(raw_path, mask_path, mode='spatial'):
+def normalize(raw_path, mask_path, mode='spatial', do_validate=False):
     raw = nib.load(raw_path)
     mask = nib.load(mask_path)
     
@@ -17,54 +17,87 @@ def normalize(raw_path, mask_path, mode='spatial'):
     
     output_path = raw_path.replace('Human_Connectome_Project', 'prep_HCP')
     non_zero_voxels = apply_mask(raw, masker_img)
+    something_wrong = [] # For validation of normalization.
     
     if mode == 'spatial':
-        normalized = []
+        if do_validate : 
+            for idx, tr in enumerate(non_zero_voxels):
+                tr = tr.reshape(-1)
+                if round(tr.mean(),4) != 0 :
+                    something_wrong.append(idx)
+                    continue
+                if round(tr.std(),4) != 1 :
+                    something_wrong.append(idx)
+                    continue
+        
+        elif not do_validate :
+            normalized = []
 
-        for tr in non_zero_voxels:
-            tr = tr.reshape(-1)
-            tr -= tr.mean()
-            tr /= tr.std()
-            normalized.append(tr[np.newaxis])
+            for tr in non_zero_voxels:
+                tr = tr.reshape(-1)
+                tr -= tr.mean()
+                tr /= tr.std()
+                normalized.append(tr[np.newaxis])
 
-        normalized = np.concatenate(normalized, axis=0)
+            normalized = np.concatenate(normalized, axis=0)
         
     elif mode == 'temporal':
-        non_zero_voxel_means = []
-        non_zero_voxel_std = []
-        ebs = 7./3 - 4./3 -1
+        if do_validate : 
+            for voxels in non_zero_voxels.transpose(1,0):
+                if round(voxels.mean(),4) != 0 :
+                    something_wrong.append(idx)
+                    continue
+                if round(voxels.std(),4) != 1 :
+                    something_wrong.append(idx)
+                    continue
+        
+        elif not do_validate :
+            non_zero_voxel_means = []
+            non_zero_voxel_std = []
+            ebs = 7./3 - 4./3 -1
 
-        for voxels in non_zero_voxels.transpose(1,0):
-            m = voxels.mean()
-            std = voxels.std()
-            std = std if std != 0 else ebs
+            for voxels in non_zero_voxels.transpose(1,0):
+                m = voxels.mean()
+                std = voxels.std()
+                std = std if std != 0 else ebs
 
-            non_zero_voxel_means.append(m)
-            non_zero_voxel_std.append(std)
+                non_zero_voxel_means.append(m)
+                non_zero_voxel_std.append(std)
 
-        non_zero_voxel_means = np.array(non_zero_voxel_means)
-        non_zero_voxel_std = np.array(non_zero_voxel_std)
-        normalized = np.array([(v - non_zero_voxel_means)/non_zero_voxel_std for v in non_zero_voxels])
-        del non_zero_voxel_means, non_zero_voxel_std, ebs
+            non_zero_voxel_means = np.array(non_zero_voxel_means)
+            non_zero_voxel_std = np.array(non_zero_voxel_std)
+            normalized = np.array([(v - non_zero_voxel_means)/non_zero_voxel_std for v in non_zero_voxels])
+            del non_zero_voxel_means, non_zero_voxel_std, ebs
         
     elif mode == 'subjectwise':
-        orig_shape = non_zero_voxels.shape
-        non_zero_voxels = non_zero_voxels.reshape(-1)
-        non_zero_voxels -= non_zero_voxels.mean()
-        non_zero_voxels /= non_zero_voxels.std()
-        normalized = non_zero_voxels.reshape(orig_shape)
+        if do_validate : 
+            non_zero_voxels = non_zero_voxels.reshape(-1)
+            
+            if round(non_zero_voxels.means(), 4) != 0 or round(non_zero_voxels.std(), 4) != 1:
+                something_wrong.append('wrong')
+        
+        elif not do_validate :
+            orig_shape = non_zero_voxels.shape
+            non_zero_voxels = non_zero_voxels.reshape(-1)
+            non_zero_voxels -= non_zero_voxels.mean()
+            non_zero_voxels /= non_zero_voxels.std()
+            normalized = non_zero_voxels.reshape(orig_shape)
         
     else :
         raise ValueError("normalization method should be among the followings : 'spatial', 'temporal', 'subjectwise'.")
         
-    normalized = unmask(normalized, masker_img)
-    nib.save(normalized, output_path)
+    if not do_validate :
+        normalized = unmask(normalized, masker_img)
+        nib.save(normalized, output_path)
     
     if raw.in_memory : raw.uncache()
     if mask.in_memory : mask.uncache()
     if normalized.in_memory : normalized.uncache()
     if masker_img.in_memory : masker_img.uncache()
-    del raw, mask, non_zero_voxels, normalized, masker, masker_img
+        
+    if do_validate :
+        return (raw_path, something_wrong)
+    del raw, mask, non_zero_voxels, normalized, masker, masker_img, something_wrong
     gc.collect()
     
 def __update(result):
