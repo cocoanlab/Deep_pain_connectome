b05cd19721c4ed6c4d65fd0de43d1947017908a5
Hahnnz <lphion96@naver.com>
2019-10-21 17:56:53 +0900

    added Deconvolution and removed tflearn's GAP

--- models/layers.py
+++ models/layers.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-from tflearn.layers.conv import global_avg_pool
 
 def conv(data, ksize, filters, ssize, padding, use_bias, conv_mode='2d' ,conv_name=None, bn_name=None, bn=False, act=True, is_train=None):
     if bn and is_train==None:
@@ -17,6 +16,29 @@ def conv(data, ksize, filters, ssize, padding, use_bias, conv_mode='2d' ,conv_na
                                   padding=padding.upper(),
                                   name=conv_name,use_bias=use_bias,
                                   kernel_initializer=tf.contrib.layers.xavier_initializer())
+    else :
+        raise ValueError('Convoltion mode : ['+conv_mode+'] is not available. plz select "2d" or "3d".')
+    
+    if bn : output = batch_norm(output, is_train, bn_name)
+    if act : output = tf.nn.relu(output)
+    return output
+
+def deconv(data, ksize, filters, ssize, padding, use_bias, deconv_mode='2d' ,deconv_name=None, bn_name=None, bn=False, act=True, is_train=None):
+    if bn and is_train==None:
+        raise ValueError('Phase should be declared "True" for train or "False" for test when you activate batch normalization.')
+    
+    if deconv_mode.lower() == '2d':
+        output = tf.layers.conv2d_transpose(data, kernel_size=ksize, filters=filters,
+                                            strides=(ssize,ssize),
+                                            padding=padding.upper(),
+                                            name=deconv_name,use_bias=use_bias,
+                                            kernel_initializer=tf.contrib.layers.xavier_initializer())
+    elif deconv_mode.lower() == '3d':
+        output = tf.layers.conv3d_transpose(data, kernel_size=ksize, filters=filters,
+                                            strides=(ssize,ssize,ssize),
+                                            padding=padding.upper(),
+                                            name=deconv_name,use_bias=use_bias,
+                                            kernel_initializer=tf.contrib.layers.xavier_initializer())
     else :
         raise ValueError('Convoltion mode : ['+conv_mode+'] is not available. plz select "2d" or "3d".')
     
@@ -78,7 +100,3 @@ def fc(data, num_out, name=None, relu=True, bn=True, is_train=None):
         if relu : 
             output = tf.nn.relu(output)
     return output
-
-def global_avg_pooling(data, name=None):
-    with tf.name_scope(name):
-        return global_avg_pool(data)
