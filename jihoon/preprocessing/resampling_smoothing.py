d376a1c694933fefc7ff30a0295d5da7fb6b0d6f
hahnnz <lphion96@naver.com>
2020-01-14 17:47:25 +0900

    added preproc func : resample 95,109,95 -> 79,95,98 and smoothing with 5 fwhm

---
+++ jihoon/preprocessing/resampling_smoothing.py
@@ -0,0 +1,46 @@
+import os
+import nibabel as nib
+
+from multiprocessing import Pool
+from nilearn.image import smooth_img, resample_to_img
+from tqdm import tqdm
+
+
+hcp_orig_dataset_root = '/media/das/Human_Connectome_Project/'
+hcp_data = []
+template = nib.load('./friend_trial01.nii')
+
+for dirpath, _, filenames in os.walk(hcp_orig_dataset_root):
+    for fname in filenames:
+        comp_dir = os.path.join(dirpath,'smooth5')
+        if not os.path.isdir(comp_dir):
+            os.mkdir(comp_dir)
+        
+        fullpath = os.path.join(dirpath,fname)
+        if 'nii.gz' in fullpath:
+            hcp_data.append(fullpath)
+            
+def resampling_smoothing(fmripath, fwhm=5):
+    savepath = fmripath.split('/')
+    fname = savepath.pop()
+    savepath = '/'.join(savepath)
+    savepath = os.path.join(savepath, 'smooth5', fname.replace('nii.gz', 'nii'))
+    
+    data = nib.load(fmripath)
+    data = resample_to_img(data, template)
+    data = smooth_img(data, fwhm)
+    nib.save(data, savepath)
+    if data.in_memory : data.uncache()
+    del data
+            
+mp = Pool(20)
+pbar = tqdm(total=len(hcp_data))
+def update(result):
+    pbar.update(1)
+    
+for path in hcp_data:
+    mp.apply_async(resampling_smoothing, args=(path,), callback=update)
+    
+mp.close()
+mp.join()
+pbar.close()
