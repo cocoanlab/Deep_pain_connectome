1a517852c675617ed99097d147c0369eef9680eb
hahnnz <lphion96@naver.com>
2020-01-15 02:30:03 +0900

    changed to get spike from raw, modified some lines

--- jihoon/preprocessing/extract_outlier_wm_csf.py
+++ jihoon/preprocessing/extract_outlier_wm_csf.py
@@ -1,47 +1,61 @@
 from multiprocessing import Pool
 from tqdm import tqdm
+import pandas as pd
 import os
+import pickle
+import itertools
 
-def extract_comp(path):
+def extract_comp(orig_path, preproc_path):
     matlab_abspath='/usr/local/bin/matlab'
-    savepath = '/'.join(path.split('/')[:-1]+['COMPS/'])
-    save_fname = path.split('/')[-1].split('.')[0]+'_compounds.mat'
+    savepath = '/'.join(preproc_path.split('/')[:-1]+['compounds/'])
+    save_fname = preproc_path.split('/')[-1].split('.')[0]+'_compounds.mat'
     
     processed = next(os.walk(savepath))[-1]
-    command = [matlab_abspath,'-nodisplay','-nodesktop','-r', f'"get_outlier_wm_csf {path} {savepath} ;exit"']
+    command = [matlab_abspath,'-singleCompThread','-nodisplay','-nodesktop','-r', f'"get_outlier_wm_csf {orig_path} {preproc_path} {savepath} ;exit"']
     if save_fname not in processed:
         error_occur = os.system(' '.join(command))
-        _ = os.remove(path.replace('.gz',''))
+        _ = os.remove(preproc_path.replace('.gz',''))
         if error_occur:
-            print(path)
+            print(preproc_path)
 
 hcp_orig_dataset_root = '/media/das/Human_Connectome_Project/'
+unpreproc = sorted(pd.read_csv('unprep_list.csv')['data_path'])
 hcp_data = []
+hcp_orig = []
 
 for dirpath, _, filenames in os.walk(hcp_orig_dataset_root):
-    comp_dir = os.path.join(dirpath,'COMPS')
-    if not os.path.isdir(comp_dir):
-        os.mkdir(comp_dir)
-        
     for fname in filenames:
+        comp_dir = os.path.join(dirpath,'compounds')
+        if not os.path.exists(comp_dir):
+            os.mkdir(comp_dir)
+        
         fullpath = os.path.join(dirpath,fname)
+                
         if 'nii.gz' in fullpath:
-            hcp_data.append(fullpath)
-            
-hcp_data = sorted(hcp_data)
+            subj_num = fullpath.split('/')[-2]
+            _, TASK, DIRECTION = fullpath.split('/')[-1].split('_')
+            DIRECTION = DIRECTION.split('.')[0]
 
-Pool(12).map(extract_comp, hcp_data)
-'''
+            for origpath in unpreproc:
+                if subj_num in origpath and TASK in origpath and DIRECTION in origpath:
+                    break
+                    
+            hcp_data.append(fullpath)
+            hcp_orig.append(origpath.replace('/home/hahnz/CNIR09','/media/cnir09'))
+            
+sorted_order = sorted(range(len(hcp_data)), key=lambda k: hcp_data[k])
+hcp_data = [hcp_data[i] for i in sorted_order]
+hcp_orig = [hcp_orig[i] for i in sorted_order]
+
+mp = Pool(36)
 pbar = tqdm(total=len(hcp_data))
-pbar.set_description('Exctract Compounds : ')
 
 def update(result):
     pbar.update(1)
-
-for path in hcp_data:
-    mp.apply_async(extract_comp, args=(path,), callback=update)
+    
+for orig, preproc in zip(hcp_orig, hcp_data):
+    mp.apply_async(extract_comp, args=(orig, preproc,), callback=update)
     
 mp.close()
 mp.join()
 pbar.close()
-'''
--- jihoon/preprocessing/get_outlier_wm_csf.m
+++ jihoon/preprocessing/get_outlier_wm_csf.m
@@ -1,22 +1,22 @@
-function [outlier, wm, csf] = get_outlier_wm_csf(fmripath, savepath)
+function [outlier, wm, csf] = get_outlier_wm_csf(orig_path, preproc_path, savepath)
 
 addpath(genpath(pwd));
 addpath(genpath('/home/hahnz/Resources/spm12'));
 
-tfmri = fmri_data(fmripath);
+orig = fmri_data(orig_path);
+tfmri = fmri_data(preproc_path);
 
 [~, components] = extract_gray_white_csf(tfmri, 'masks', {which('gray_matter_mask.img'),...
 which('canonical_white_matter_thrp5_ero1.nii'), ...
 which('canonical_ventricles_thrp5_ero1.nii')});
 
-outlier = preprocess(tfmri, 'outliers_rmssd').covariates;
+outlier = preprocess(orig, 'outliers_rmssd').covariates;
 wm = scale(double(components{2}));
 csf = scale(double(components{3}));
 
-fname = split(fmripath, '/');
+fname = split(preproc_path, '/');
 fname = split(fname{end}, '.');
 fname = fname{1}+"_compounds.mat";
 
+clear orig tfmri components preproc_path orig_path
 save(savepath+fname);
-clear tfmri components fmripath fname savepath;
-return;
