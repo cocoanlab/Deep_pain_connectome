fac76ab3003483fe2c46a7d1cdd7c021232c680d
hahnnz <lphion96@naver.com>
2020-01-21 16:57:07 +0900

    modified confounds extractor

--- jihoon/preprocessing/extract_outlier_wm_csf.py
+++ jihoon/preprocessing/extract_outlier_wm_csf.py
@@ -3,58 +3,44 @@ from tqdm import tqdm
 import pandas as pd
 import os
 
-def extract_comp(orig_path, preproc_path):
+def extract_comp(preproc_path):
     matlab_abspath='/usr/local/bin/matlab'
     save_fname = preproc_path.split('/')[-1].split('.')[0]+'_compounds.mat'
     savepath = '/'.join(preproc_path.replace('smooth5','compounds').split('/')[:-1])+'/'
+    
+    orig_raw_path = preproc_path.replace('compounds','unprocessed')
+    
     if not os.path.exists(savepath):
         os.mkdir(savepath)
     
     processed = next(os.walk(savepath))[-1]
-    command = ['nohup',matlab_abspath,'-singleCompThread','-nodisplay','-nodesktop','-r', f'"get_outlier_wm_csf {orig_path} {preproc_path} {savepath} ;exit" > out.out']
+    command = ['nohup',matlab_abspath,
+               '-singleCompThread','-nodisplay','-nodesktop','-r', 
+               f'"get_outlier_wm_csf {orig_raw_path} {preproc_path} {savepath} ;exit" >/dev/null 2>&1']
+    
     if save_fname not in processed:
         error_occur = os.system(' '.join(command))
-        #_ = os.remove(preproc_path.replace('.gz',''))
         if error_occur:
             print(preproc_path)
-
+            
 hcp_orig_dataset_root = '/media/das/Human_Connectome_Project/'
-unpreproc = sorted(pd.read_csv('unprep_list.csv')['data_path'])
 hcp_data = []
-hcp_orig = []
 
 for dirpath, _, filenames in os.walk(hcp_orig_dataset_root):
     for fname in filenames:
         fullpath = os.path.join(dirpath,fname)
-                
-        if 'nii' in fullpath and 'smooth5' in fullpath:
-            subj_num = fullpath.split('/')[-3]
-            _, TASK, DIRECTION = fullpath.split('/')[-1].split('_')
-            DIRECTION = DIRECTION.split('.')[0]
-
-            for origpath in unpreproc:
-                if all([subj_num in origpath, TASK in origpath, DIRECTION in origpath]):
-                    break
-            else:
-                break
-                    
+        orig_path_check = fullpath.replace('smooth5','unprocessed')
+        if 'nii' in fullpath and 'smooth5' in fullpath and os.path.exists(orig_path_check):
             hcp_data.append(fullpath)
-            hcp_orig.append(origpath.replace('/home/hahnz/CNIR09','/media/cnir09'))
-            
-sorted_order = sorted(range(len(hcp_data)), key=lambda k: hcp_data[k])
-start = 0
-end = len(hcp_data)
-hcp_data = [hcp_data[i] for i in sorted_order][start:end]
-hcp_orig = [hcp_orig[i] for i in sorted_order][start:end]
 
-mp = Pool(18)
+mp = Pool(20)
 pbar = tqdm(total=len(hcp_data))
 
 def update(result):
     pbar.update(1)
     
-for orig, preproc in zip(hcp_orig, hcp_data):
-    mp.apply_async(extract_comp, args=(orig, preproc,), callback=update)
+for path in hcp_data:
+    mp.apply_async(extract_comp, args=(path,), callback=update)
     
 mp.close()
 mp.join()
--- jihoon/preprocessing/get_outlier_wm_csf.m
+++ jihoon/preprocessing/get_outlier_wm_csf.m
@@ -1,24 +1,25 @@
 function [outlier, wm, csf] = get_outlier_wm_csf(orig_path, preproc_path, save_path)
 
-maxNumCompThreads = 1;
-
 addpath(genpath(pwd));
-addpath(genpath('/home/hahnz/Resources/spm12'));
+addpath(genpath('/usr/local/Resources/spm12'));
 
-orig = fmri_data(orig_path);
 tfmri = fmri_data(preproc_path);
-
 [~, components] = extract_gray_white_csf(tfmri, 'masks', {which('gray_matter_mask.img'),...
 which('canonical_white_matter_thrp5_ero1.nii'), ...
 which('canonical_ventricles_thrp5_ero1.nii')});
+tfmri = [];
 
+orig = fmri_data(orig_path);
 outlier = preprocess(orig, 'outliers_rmssd').covariates;
+orig = [];
+
 wm = scale(double(components{2}));
 csf = scale(double(components{3}));
+components = [];
 
 fname = split(preproc_path, '/');
 fname = split(fname{end}, '.');
 fname = fname{1}+"_compounds.mat";
 
-clear orig tfmri components preproc_path orig_path;
+clear preproc_path orig_path;
 save(save_path+fname);
