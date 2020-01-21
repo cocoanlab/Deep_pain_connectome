510789a75637b096a40c1a7b3fd5ce46854724a2
hahnnz <lphion96@naver.com>
2020-01-21 16:57:45 +0900

    modified resampling function to fit bmrk3 affine

--- jihoon/preprocessing/resampling_smoothing.py
+++ jihoon/preprocessing/resampling_smoothing.py
@@ -2,14 +2,14 @@ import os
 import nibabel as nib
 
 from multiprocessing import Pool
-from nilearn.image import smooth_img, resample_to_img
+from nilearn.image import smooth_img, resample_img
 from tqdm import tqdm
 
 
 hcp_orig_dataset_root = '/media/das/Human_Connectome_Project/'
 hcp_data = []
 template = nib.load('./friend_trial01.nii')
-
+'''
 for dirpath, _, filenames in os.walk(hcp_orig_dataset_root):
     for fname in filenames:
         comp_dir = os.path.join(dirpath,'smooth5')
@@ -19,6 +19,18 @@ for dirpath, _, filenames in os.walk(hcp_orig_dataset_root):
         fullpath = os.path.join(dirpath,fname)
         if 'nii.gz' in fullpath:
             hcp_data.append(fullpath)
+'''
+for dirpath, _, filenames in os.walk(hcp_orig_dataset_root):
+    for fname in filenames:
+        comp_dir = os.path.join(dirpath,'smooth5')
+        if not os.path.isdir(comp_dir):
+            os.mkdir(comp_dir)
+        
+        fullpath = os.path.join(dirpath,fname)
+        if 'unpro' in fullpath:
+            fullpath = fullpath.replace('/unprocessed/', '/')
+            fullpath = fullpath.replace('nii', 'nii.gz')
+            hcp_data.append(fullpath)
             
 def resampling_smoothing(fmripath, fwhm=5):
     savepath = fmripath.split('/')
@@ -27,7 +39,7 @@ def resampling_smoothing(fmripath, fwhm=5):
     savepath = os.path.join(savepath, 'smooth5', fname.replace('nii.gz', 'nii'))
     
     data = nib.load(fmripath)
-    data = resample_to_img(data, template)
+    data = resample_img(data, template.get_affine(), template.shape, force_resample=True)
     data = smooth_img(data, fwhm)
     nib.save(data, savepath)
     if data.in_memory : data.uncache()
